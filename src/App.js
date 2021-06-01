e1fe2a558c4a3d1a2acb3324cb84e99f0489d6e5
Jacque S <jacqueds.git@gmail.com>
2021-06-01 16:57:07 -0700

    moved app outside of components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./components/home/Home"
+function App() {
+  
+  return (
+    <div className="position-relative"style={{backgroundColor: "#042F75"}}>
+      <Home/> 
+    </div>
+  );
+}
+
+export default App;
